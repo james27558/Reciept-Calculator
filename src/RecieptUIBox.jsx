995500544e9816faf45e97e0372316508d91dd48
p40420jc <james.coffey@student.manchester.ac.uk>
2024-07-14 19:35:02 +0100

    Remove old commented out testing code

--- src/RecieptUIBox.jsx
+++ src/RecieptUIBox.jsx
@@ -4,25 +4,7 @@ import RecieptUIRow from "./RecieptUIRow";
 
 
 export default function RecieptUIBox({recieptNo}) {
-    const [st, setSt] = useState(["start"])
-
-    console.log("rerender");
-
-    function add() {
-        setSt([...st, "new"])
-    }
-    
-    let unfoldSt = st.map((e) => <> {e} </>)
-
     return (
-        // <>
-        //     <p>
-        //         {unfoldSt}
-        //     </p>
-
-        //     <button onClick={add} value="Click"> Click </button>
-        // </>
-
         <label htmlFor=""> Reciept No. {recieptNo}
             <RecieptUIRow name="Nacoya" />
         </label>
